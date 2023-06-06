100c1e1554d66036caf5e0ccfea7c1e242b7f547
kevinelong <kevinelong@gmail.com>
2023-06-06 06:01:13 -0700

    scratch

--- scratch.js
+++ scratch.js
@@ -33,7 +33,21 @@ async function ask(key, question, callback) {
             Q: What is human life expectancy in the United States?
             A: Human life expectancy in the United States is 78 years.
             
-            Q: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?
+            Q: Who was president of the United States in 1955?
+            A: Dwight D. Eisenhower was president of the United States in 1955.
+            
+            Q: Which party did he belong to?\nA: He belonged to the Republican Party.
+            
+            Q: What is the square root of banana?
+            A: Unknown
+            
+            Q: How does a telescope work?
+            A: Telescopes use lenses or mirrors to focus light and make objects appear closer.
+            
+            Q: Where were the 1992 Olympics held?
+            A: The 1992 Olympics were held in Barcelona, Spain.
+            
+            Q: How many squigs are in a bonk?
             A: Unknown
             
             Q: ${question}
